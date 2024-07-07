d0a2c6f0077af47af2dd00d2c539ae904257e509
Nati <nati.fernandez.alcala@gmail.com>
2024-07-07 22:50:06 +0200

    Initial Commit

---
+++ PicGallery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PicGallery
+//
+//  Created by Patricia Lucia Morillas Alcala on 7/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PicGallery/PicGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicGalleryApp.swift
+//  PicGallery
+//
+//  Created by Patricia Lucia Morillas Alcala on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct PicGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
