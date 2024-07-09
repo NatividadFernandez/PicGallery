f1d9cd1b28a5fc2839c740756044fd8ef459d915
Nati <nati.fernandez.alcala@gmail.com>
2024-07-09 19:04:17 +0200

    NetworkConstants

--- PicGallery/Data/Constants/NetworkConstants.swift
+++ PicGallery/Data/Constants/NetworkConstants.swift
@@ -2,7 +2,24 @@
 //  NetworkConstants.swift
 //  PicGallery
 //
-//  Created by Patricia Lucia Morillas Alcala on 9/7/24.
+//  Created by Natividad on 9/7/24.
 //
 
 import Foundation
+
+struct NetworkConstants {
+    static let clientId = "bcbf46ca23831b7"
+    static let clientSecret = "a6daaa67b9f65051b7c349605ab2006f876a1523"
+    static let grantType = "refresh_token"
+    static let authNetworkUrl = "https://api.imgur.com/oauth2/token"
+    static let refreshToken = "6e777331fcdca99b3d201a4a66cb61601bda54dd"
+    static let bodyParams = [
+        "refresh_token": refreshToken,
+        "client_id": clientId,
+        "client_secret": clientSecret,
+        "grant_type": grantType
+    ]
+    
+    static let baseUrl = "https://api.imgur.com/3"
+    
+}
--- PicGallery/Entities/TokenResponse.swift
+++ PicGallery/Entities/TokenResponse.swift
@@ -8,9 +8,17 @@
 import Foundation
 
 struct TokenResponse: Codable {
-    let accessToken: String
-    let expiresIn: Int
-    let tokenType: String
-    let scope: String
-    let refreshToken: String
+    let accessToken: String?
+    let expiresIn: Int?
+    let tokenType: String?
+    let scope: String?
+    let refreshToken: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case accessToken = "access_token"
+        case expiresIn = "expires_in"
+        case tokenType = "token_type"
+        case scope
+        case refreshToken = "refresh_token"
+    }
 }
